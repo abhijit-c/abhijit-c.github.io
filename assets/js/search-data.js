2ee6b152f1bf1a8b8c0c7d148ace591bc864d21e
abhijit-c <abhijit9331@gmail.com>
2025-05-06 21:44:53 +0000

    Deploying to gh-pages from @ abhijit-c/abhijit-c.github.io@d2227ce1f2684b632c4283da9e0869f2a7494347 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -138,25 +138,11 @@ ninja.data = [{
           window.open("https://github.com/abhijit-c", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-orcid',
         title: 'ORCID',
         section: 'Socials',
         handler: () => {
           window.open("https://orcid.org/0000-0002-5092-3503", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
